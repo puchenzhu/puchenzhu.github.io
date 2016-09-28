f79639478036301204e245529f922b1913d78874
Nerssi <arya@live.ca>
2016-09-28 14:21:02 -0700

    Added social media icons, fixed nav underline colors.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ $(document).ready(function () {
 
   $("#title").hide().slideDown(1000);
 
+  $(".nav a").mouseover(function () {
+    $(this).children().css("border-bottom", "2px solid " + $(this).children().css("color"));
+  });
+
+  $(".nav a").mouseout(function () {
+    if ($(this).children().hasClass("selected"))
+      return;
+    $(this).children().css("border-bottom", "none");
+  });
 });
